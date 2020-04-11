786afe9ed97449a46fb0ba6b5892e82f72248bbb
ok-nc <omar.khatib@gmail.com>
2020-04-11 12:10:16 -0400

    Got rid of pre-training, and all associated pretraining functions.

--- network_model.py
+++ network_model.py
@@ -106,15 +106,12 @@ class Forward(nn.Module):
         # out = self.linears[-1](out)
             # print(out.size())
             # out = torch.sigmoid(out)
-            # last_Lor_layer = out
-            # return last_Lor_layer, last_Lor_layer
+
         # If use lorentzian layer, pass this output to the lorentzian layer
         if self.use_lorentz:
-            # last_Lor_layer = out[:, :-1]
             # NOTE: if using pretraining, below must be commented out, otherwise initial fit is worse
             # out = torch.sigmoid(out)            # Lets say w0, wp is in range (0,5) for now
             out = F.relu(out) + 0.00001
-            last_Lor_layer = out[:, :-1]
 
             # Get the out into (batch_size, num_lorentz, 3) and the last epsilon_inf baseline
             # epsilon_inf = out[:,-1]  # For debugging purpose now
@@ -138,7 +135,6 @@ class Forward(nn.Module):
             # self.wps = wp.data.cpu().numpy()
             # self.gs = g.data.cpu().numpy()
 
-            #print(last_Lor_layer.size())
             #print(g.size())
             # self.eps_inf = epsilon_inf.data.cpu().numpy()
 
@@ -235,7 +231,7 @@ class Forward(nn.Module):
             # print("T size",T.size())
             # Last step, sum up except for the 0th dimension of batch_size (deprecated since we sum at e above)
             # T = torch.sum(T, 1).float()
-            return T, last_Lor_layer
+            return T
 
         # The normal mode to train without Lorentz
         if self.use_conv:
@@ -247,133 +243,7 @@ class Forward(nn.Module):
             # Final touch, because the input is normalized to [-1,1]
             # S = tanh(out.squeeze())
             out = out.squeeze()
-        return out, out
-
-
-def Lorentz_layer(Lorentz_params):
-    # last_Lor_layer = out[:, :-1]
-    # NOTE: if using pretraining, below must be commented out, otherwise initial fit is worse
-    # out = torch.sigmoid(out)            # Lets say w0, wp is in range (0,5) for now
-    # out = F.relu(out) + 0.00001
-
-    # This block of code redefines 'self' variables from model
-    normalize_input = True
-    geoboundary = [20, 200, 20, 100]
-    fre_low = 0.5
-    fre_high = 5
-    num_lorentz = 4
-    num_spec_point = 300
-    w_numpy = np.arange(fre_low, fre_high, (fre_high - fre_low) / num_spec_point)
-    w0 = torch.tensor(np.arange(0, 5, 5 / num_lorentz))
-
-    # Create the tensor from numpy array
-    cuda = True if torch.cuda.is_available() else False
-    if cuda:
-        w = torch.tensor(w_numpy).cuda()
-        w0 = w0.cuda()
-    else:
-        w = torch.tensor(w_numpy)
-
-    # Get the out into (batch_size, num_lorentz, 3) and the last epsilon_inf baseline
-    # epsilon_inf = Lorentz_params[:, -1]  # For debugging purpose now
-
-    # Set epsilon_inf to be a constant universal value here
-    # epsilon_inf = torch.tensor([10], requires_grad=False).expand_as(epsilon_inf)
-    # if torch.cuda.is_available():
-    #     epsilon_inf = epsilon_inf.cuda()
-    Lorentz_params = Lorentz_params.view([-1, int(Lorentz_params.size(1) / 3), 3])
-
-    # Get the list of params for lorentz, also add one extra dimension at 3rd one to0
-    w0 = Lorentz_params[:, :, 0].unsqueeze(2) * 5
-    wp = Lorentz_params[:, :, 1].unsqueeze(2) * 5
-    g = Lorentz_params[:, :, 2].unsqueeze(2) * 0.5
-    # nn.init.xavier_uniform_(g)
-    # This is for debugging purpose (Very slow), recording the output tensors
-    # self.w0s = w0.data.cpu().numpy()
-    # self.wps = wp.data.cpu().numpy()
-    # self.gs = g.data.cpu().numpy()
-
-    # print(last_Lor_layer.size())
-    # print(g.size())
-    #self.eps_inf = epsilon_inf.data.cpu().numpy()
-
-    # Expand them to the make the parallelism, (batch_size, #Lor, #spec_point)
-    w0 = w0.expand(Lorentz_params.size(0), num_lorentz, num_spec_point)
-    wp = wp.expand_as(w0)
-    g = g.expand_as(w0)
-    w_expand = w.expand_as(g)
-    """
-    Testing code
-    #print("c1 size", self.c1.size())
-    #print("w0 size", w0.size())
-    End of testing module
-    """
-    # Get the powers first
-    # w02 = pow(w0, 2)
-    # wp2 = pow(wp, 2)
-    # w2 = pow(w_expand, 2)
-    # g2 = pow(g, 2)
-    #
-    # # Start calculating
-    # s1 = add(w02, -w2)
-    # s12 = pow(s1, 2)
-    # n1 = mul(wp2, s1)
-    # n2 = mul(wp2, mul(w_expand, g))
-    # denom = add(s12, mul(w2, g2))
-    # e1 = div(n1, denom)
-    # e2 = div(n2, denom)
-
-    # # This is the version of more "machine" code that hard to understand but much more memory efficient
-    # e1 = div(mul(pow(wp, 2), add(pow(w0, 2), -pow(w_expand, 2))),
-    #          add(pow(add(pow(w0, 2), -pow(w_expand, 2)), 2), mul(pow(w_expand, 2), pow(g, 2))))
-    e2 = div(mul(pow(wp, 2), mul(w_expand, g)),
-             add(pow(add(pow(w0, 2), -pow(w_expand, 2)), 2), mul(pow(w_expand, 2), pow(g, 2))))
-    # # End line for the 2 versions of code that do the same thing, 1 for memory efficient but ugly
-
-
-    # self.e2 = e2.data.cpu().numpy()                 # This is for plotting the imaginary part
-    # self.e1 = e1.data.cpu().numpy()                 # This is for plotting the imaginary part
-    """
-    debugging purposes: 2019.12.10 Bens code for debugging the addition of epsilon_inf
-    print("size of e1", e1.size())
-    print("size pf epsilon_inf", epsilon_inf.size())
-    """
-    # the correct calculation should be adding up the es
-    # e1 = torch.sum(e1, 1)
-    e2 = torch.sum(e2, 1)
-
-    # epsilon_inf = epsilon_inf.unsqueeze(1).expand_as(e1)  # Change the shape of the epsilon_inf
-
-    # e1 += epsilon_inf
-    #
-    # # print("e1 size", e1.size())
-    # # print("e2 size", e2.size())
-    # e12 = pow(e1, 2)
-    # e22 = pow(e2, 2)
-    #
-    # n = sqrt(0.5 * add(sqrt(add(e12, e22)), e1))
-    # k = sqrt(0.5 * add(sqrt(add(e12, e22)), -e1))
-    # n_12 = pow(n + 1, 2)
-    # k2 = pow(k, 2)
-
-    # T without absorption
-    # T = div(4*n, add(n_12, k2)).float()
-
-    # d, _ = torch.max(G[:, 4:], dim=1)
-    # # print(d)
-    #
-    # if normalize_input:
-    #     d = d * (geoboundary[-1] - geoboundary[-2]) * 0.5 + (
-    #                 geoboundary[-1] + geoboundary[-2]) * 0.5
-    # # print(d)
-    # # print(d.size())
-    # d = d.unsqueeze(1).expand_as(k)
-    # # print(d.size())
-    # ab = torch.exp(-0.0005 * 4 * math.pi * mul(d, k))
-    # T_coeff = div(4 * n, add(n_12, k2))
-    # # T = mul(T_coeff, ab).float()
-    T = e2.float()
-
+        return out
 
     """
     Debugging and plotting (This is very slow, comment to boost)
